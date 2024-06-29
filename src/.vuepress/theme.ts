97cb23b61a493628927d114fadb680e3750a0043
FoskyM <i@fosky.top>
2024-06-29 19:41:45 +0800

    add `Giscus` for comments

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -46,14 +46,13 @@ export default hopeTheme({
 
   // 在这里配置主题提供的插件
   plugins: {
-    // 注意: 仅用于测试! 你必须自行生成并在生产环境中使用自己的评论服务
-    // comment: {
-    //   provider: "Giscus",
-    //   repo: "vuepress-theme-hope/giscus-discussions",
-    //   repoId: "R_kgDOG_Pt2A",
-    //   category: "Announcements",
-    //   categoryId: "DIC_kwDOG_Pt2M4COD69",
-    // },
+    comment: {
+      provider: "Giscus",
+      repo: "FoskyM/Note",
+      repoId: "R_kgDOMPq9JA",
+      category: "Comments",
+      categoryId: "DIC_kwDOMPq9JM4Cgd_6",
+    },
 
     components: {
       components: ["Badge", "VPCard"],
